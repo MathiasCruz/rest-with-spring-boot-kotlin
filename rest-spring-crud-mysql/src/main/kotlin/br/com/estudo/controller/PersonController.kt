4ca572f3f9b8d3b6c0e90620e87a02dff49ca067
MathiasCruz <mathiasaugustodacruz@gmail.com>
2025-03-22 16:16:15 -0300

    CRUD mockado para o objeto Person

--- rest-spring-crud-mysql/src/main/kotlin/br/com/estudo/controller/PersonController.kt
+++ rest-spring-crud-mysql/src/main/kotlin/br/com/estudo/controller/PersonController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import br.com.estudo.service.PersonService
+import org.springframework.web.bind.annotation.RequestBody
 
 @RestController
 @RequestMapping("/person")
@@ -22,5 +23,36 @@ class PersonController {
         )
     fun findById(@PathVariable(value="id") id : Long): Person {
         return service.findById(id)
+    }
+
+    @RequestMapping(value = ["/{id}"],
+        method = [RequestMethod.DELETE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+        )
+    fun deleteById(@PathVariable(value="id") id : Long) {
+        return service.delete(id)
+    }
+
+    @RequestMapping(method = [RequestMethod.GET],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+        )
+    fun findAll(): List<Person> {
+        return service.findAll()
+    }
+
+    @RequestMapping(method = [RequestMethod.POST],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+        )
+    fun createPerson(@RequestBody person: Person): Person{
+        return service.create(person)
+    }
+
+    @RequestMapping(method = [RequestMethod.PUT],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+        )
+    fun updatePerson(@RequestBody person: Person): Person{
+        return service.update(person)
     }
 }
--- rest-spring-crud-mysql/src/main/kotlin/br/com/estudo/service/PersonService.kt
+++ rest-spring-crud-mysql/src/main/kotlin/br/com/estudo/service/PersonService.kt
@@ -20,4 +20,39 @@ class PersonService {
             "Masculino"
         )
     }
+
+    fun findAll(): List<Person> {
+        logger.info("findAll")
+        val listPersons:MutableList<Person> = ArrayList()
+
+        for (i in 0..7) {
+            val person = mockPerson(i)
+            listPersons.add(person)
+        }
+        return listPersons
+    }
+
+    private fun mockPerson(i: Int): Person {
+        return Person(
+            id = counter.incrementAndGet(),
+            "FistName $i",
+            "LastName $i",
+            "Some City $i",
+            "Masculino"
+        )
+    }
+
+    fun create(person: Person): Person {
+        logger.info("create")
+        return person
+    }
+
+    fun update(person: Person): Person {
+        logger.info("update")
+        return person
+    }
+
+    fun delete(id: Long) {
+        logger.info("delete")
+    }
 }
