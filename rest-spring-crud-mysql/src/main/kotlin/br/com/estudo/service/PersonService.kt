90c894be038a1597199b535a042d7abdf4bd82ab
MathiasCruz <mathiasaugustodacruz@gmail.com>
2025-03-23 14:22:47 -0300

    Refatoração do serviço e model de person para o uso de hibernate com MYSQL

--- rest-spring-crud-mysql/src/main/kotlin/br/com/estudo/service/PersonService.kt
+++ rest-spring-crud-mysql/src/main/kotlin/br/com/estudo/service/PersonService.kt
@@ -1,58 +1,41 @@
 package br.com.estudo.service
 
+import br.com.estudo.exception.ResourceNotFoundException
 import br.com.estudo.model.Person
+import br.com.estudo.repository.PersonRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.concurrent.atomic.AtomicLong
 import java.util.logging.Logger
 
 @Service
 class PersonService {
-    private val counter = AtomicLong()
+    @Autowired
+    private lateinit var repository: PersonRepository
+
     private val logger = Logger.getLogger(PersonService::class.java.name)
 
     fun findById(id: Long): Person {
         logger.info("findById: $id")
-        return Person(
-            id = counter.incrementAndGet(),
-            "Mathias",
-            "Silva",
-            "Belo Horizonte",
-            "Masculino"
-        )
+        return repository.findById(id).orElseThrow(){ResourceNotFoundException("Person not found for this id :: $id")}
     }
 
     fun findAll(): List<Person> {
         logger.info("findAll")
-        val listPersons:MutableList<Person> = ArrayList()
-
-        for (i in 0..7) {
-            val person = mockPerson(i)
-            listPersons.add(person)
-        }
-        return listPersons
+        return repository.findAll()
     }
-
-    private fun mockPerson(i: Int): Person {
-        return Person(
-            id = counter.incrementAndGet(),
-            "FistName $i",
-            "LastName $i",
-            "Some City $i",
-            "Masculino"
-        )
-    }
-
     fun create(person: Person): Person {
         logger.info("create")
-        return person
+        return repository.save(person)
     }
 
     fun update(person: Person): Person {
         logger.info("update")
-        return person
+        return repository.save(person)
     }
 
     fun delete(id: Long) {
         logger.info("delete")
+        return repository.deleteById(id)
     }
 }
