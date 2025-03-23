07d5d9d86e361fade2575ddbf49c5e58127dced5
MathiasCruz <mathiasaugustodacruz@gmail.com>
2025-03-23 18:23:41 -0300

    Refatoração das notations para os metódos HTTP

--- rest-spring-crud-mysql/src/main/kotlin/br/com/estudo/controller/PersonController.kt
+++ rest-spring-crud-mysql/src/main/kotlin/br/com/estudo/controller/PersonController.kt
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import br.com.estudo.service.PersonService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 
 @RestController
@@ -17,41 +21,32 @@ class PersonController {
     @Autowired
     private  lateinit var service: PersonService
 
-    @RequestMapping(value = ["/{id}"],
-        method = [RequestMethod.GET],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-        )
+    @GetMapping(value = ["/{id}"],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
     fun findById(@PathVariable(value="id") id : Long): Person {
         return service.findById(id)
     }
 
-    @RequestMapping(value = ["/{id}"],
-        method = [RequestMethod.DELETE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-        )
-    fun deleteById(@PathVariable(value="id") id : Long) {
-        return service.delete(id)
+    @DeleteMapping(value = ["/{id}"],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun deleteById(@PathVariable(value="id") id : Long):ResponseEntity<*> {
+        service.delete(id)
+        return ResponseEntity.noContent().build<Any>()
     }
 
-    @RequestMapping(method = [RequestMethod.GET],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-        )
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     fun findAll(): List<Person> {
         return service.findAll()
     }
 
-    @RequestMapping(method = [RequestMethod.POST],
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-        )
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
     fun createPerson(@RequestBody person: Person): Person{
         return service.create(person)
     }
 
-    @RequestMapping(method = [RequestMethod.PUT],
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-        )
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
     fun updatePerson(@RequestBody person: Person): Person{
         return service.update(person)
     }
